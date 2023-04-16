4d0579524951342e8a1d2430a3c4fef0a319bf09
Ismael Junior <ismaeljcjunior@gmail.com>
2023-04-16 18:36:17 -0300

    feat

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,72 +1,74 @@
 import React, { useState } from 'react'
-import {
-    Typography,
-    TextField,
-    Button,
-    Box,
-    Container,
-} from '@mui/material'
+import { Typography, TextField, Button, Box, Container } from '@mui/material'
+import axios from 'axios'
+const API_URL = 'https://example.com/api'
 
 export const LoginPage = () => {
+    const [isCreatingAccount, setIsCreatingAccount] = useState(false)
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
+    const [name, setName] = useState('')
+    const [errorMessage, setErrorMessage] = useState('')
 
     const handleSubmit = async (event: React.FormEvent) => {
         event.preventDefault()
-        console.log(`username: ${username}, password: ${password}`)
-
+        setErrorMessage('')
+        try {
+            if (isCreatingAccount) {
+                const response = await axios.post(`${API_URL}/users`, {
+                    username,
+                    password,
+                    name,
+                })
+                console.log(response.data)
+                // Redirect to success page
+            } else {
+                const response = await axios.post(`${API_URL}/auth`, {
+                    username,
+                    password,
+                })
+                console.log(response.data)
+                // Redirect to success page
+            }
+        } catch (error) {
+            setErrorMessage('Invalid username or password')
+            console.error(error)
+        }
     }
-
     return (
         <Container maxWidth="sm">
             <Box
-                sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    mt: 8,
-                }}
+                sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mt: 8, }}
             >
                 <Typography component="h1" variant="h5">
-                    Login
+                    {isCreatingAccount ? 'Create account' : 'Login'}
                 </Typography>
                 <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
-                    <TextField
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="username"
-                        label="Nome de usuário"
-                        name="username"
-                        autoComplete="username"
-                        autoFocus
-                        value={username}
-                        onChange={(event) => setUsername(event.target.value)}
+                    {isCreatingAccount && (
+                        <TextField margin="normal" fullWidth id="name" label="Name" name="name" autoComplete="name" autoFocus value={name} onChange={(event) => setName(event.target.value)}
+                        />
+                    )}
+                    <TextField margin="normal" fullWidth id="username" label="Username" name="username" autoComplete="username" autoFocus value={username} onChange={(event) => setUsername(event.target.value)}
                     />
-                    <TextField
-                        margin="normal"
-                        required
-                        fullWidth
-                        name="password"
-                        label="Senha"
-                        type="password"
-                        id="password"
-                        autoComplete="current-password"
-                        value={password}
-                        onChange={(event) => setPassword(event.target.value)}
+                    <TextField margin="normal" fullWidth name="password" label="Password" type="password" id="password" autoComplete="current-password" value={password} onChange={(event) => setPassword(event.target.value)}
                     />
+                    {errorMessage && (
+                        <Typography variant="body2" color="error" sx={{ mt: 1 }}>
+                            {errorMessage}
+                        </Typography>
+                    )}
                     <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        sx={{ mt: 3, mb: 2 }}
+                        type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}          >
+                        {isCreatingAccount ? 'Create account' : 'Login'}
+                    </Button>
+                    <Button
+                        fullWidth variant="outlined" onClick={() => setIsCreatingAccount(!isCreatingAccount)}
                     >
-                        Entrar
+                        {isCreatingAccount ? 'Back to Login' : 'Create Account'}
+
                     </Button>
                 </Box>
             </Box>
         </Container>
     )
 }
-
-
